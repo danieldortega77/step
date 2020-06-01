c51bff658eba8e536520e1a70eb3323790fd7973
Daniel O <danieldortega77>
2020-06-01 13:52:37 -0700

    Modify DataServlet to return a JSON string of comments

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,7 +14,9 @@
 
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,11 +26,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-  private List<String> comments;
+  private ArrayList<String> comments;
 
   @Override
   public void init() {
-    comments = new ArrayList<>();
+    comments = new ArrayList<String>();
     comments.add("What a great portfolio!");
     comments.add("Looks great. I like the theme.");
     comments.add("Daniel Ortega seems like a very cool person.");
@@ -36,43 +38,17 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Daniel!");
-  }
-}
+    // Convert the comments to JSON
+    String json = convertToJson(comments);
 
-
-/** Servlet that returns a random quote. */
-@WebServlet("/random-quote")
-public final class RandomQuoteServlet extends HttpServlet {
-
-  private List<String> quotes;
-
-  @Override
-  public void init() {
-    quotes = new ArrayList<>();
-    quotes.add(
-        "A ship in port is safe, but that is not what ships are for. "
-            + "Sail out to sea and do new things. - Grace Hopper");
-    quotes.add("They told me computers could only do arithmetic. - Grace Hopper");
-    quotes.add("A ship in port is safe, but that's not what ships are built for. - Grace Hopper");
-    quotes.add("It is much easier to apologise than it is to get permission. - Grace Hopper");
-    quotes.add("If you can't give me poetry, can't you give me poetical science? - Ada Lovelace");
-    quotes.add("I am in a charming state of confusion. - Ada Lovelace");
-    quotes.add(
-        "The Analytical Engine weaves algebraic patterns, "
-            + "just as the Jacquard loom weaves flowers and leaves. - Ada Lovelace");
-    quotes.add(
-        "Sometimes it is the people no one can imagine anything of "
-            + "who do the things no one can imagine. - Alan Turing");
-    quotes.add("Those who can imagine anything, can create the impossible. - Alan Turing");
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String quote = quotes.get((int) (Math.random() * quotes.size()));
-
-    response.setContentType("text/html;");
-    response.getWriter().println(quote);
+  private String convertToJson(ArrayList comments) {
+    Gson gson = new Gson();
+    String json = gson.toJson(comments);
+    return json;
   }
 }
