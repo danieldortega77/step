ee272d43ec8dc65811f105f06896fb184ed5a8d7
Daniel O <danieldortega77>
2020-06-01 15:23:57 -0700

    Create comment class

---
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -0,0 +1,29 @@
+package com.google.sps.data;
+
+import java.util.Date;
+
+/** Class representing a comment */
+public class Comment {
+
+  private final String text;
+  private final String author;
+  private final Date time;
+
+  public Comment(String text, String currentauthor, Date time) {
+    this.text = text;
+    this.author = author;
+    this.time = time;
+  }
+  
+  public String getText() {
+      return text;
+  }
+
+  public String getAuthor() {
+      return author;
+  }
+
+  public Date getTime() {
+      return time;
+  }
+}
