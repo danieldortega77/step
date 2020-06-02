c5e8b912183c8b9665238f0dfd0fae0f74f3f4d5
Daniel O <danieldortega77>
2020-06-02 22:00:49 +0000

    Remove "ID" field from Comment class because it is now unnecessary

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -5,27 +5,21 @@ import java.util.Date;
 /** Class representing a comment */
 public class Comment {
 
-  private final long id;
   private final String text;
   private final String author;
   private final Date time;
 
-  public Comment(long id, String text, String author) {
-    this.id = id;
+  public Comment(String text, String author) {
     this.text = text;
     this.author = author;
     this.time = new Date();
   }
 
-  public Comment(long id, String text, String author, Date time) {
+  public Comment(String text, String author, Date time) {
     this.id = id;
     this.text = text;
     this.author = author;
     this.time = time;
-  }
-
-  public long getID() {
-      return id;
   }
   
   public String getText() {
