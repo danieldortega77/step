16d594e1a2dbfc5ac4b4f42cb32d551d59e8ee11
Daniel O <danieldortega77>
2020-06-09 18:16:54 +0000

    Create map element in aboutme page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -130,3 +130,10 @@ async function displayNickname() {
     element.innerHTML = 'Your current nickname is ' + nickname + '.';
   }
 }
+
+function createMap() {
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
