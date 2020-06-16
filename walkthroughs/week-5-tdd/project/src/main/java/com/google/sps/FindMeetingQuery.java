5e230b8a34d3830d38635b223b40a17aa10d4daa
Daniel O <danieldortega77>
2020-06-16 23:21:04 +0000

    Part 2

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,20 +15,51 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public final class FindMeetingQuery {
   private static final int TIME_0830AM = TimeRange.getTimeInMinutes(8, 30);
   private static final int TIME_0900AM = TimeRange.getTimeInMinutes(9, 0);
 
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    List<TimeRange> output = Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, false));
+
     for (String attendee : request.getAttendees()) {
       for (Event event : events) {
         if (event.getAttendees().contains(attendee)) {
           TimeRange existingMeeting = event.getWhen();
+
+          for (TimeRange possibleMeeting : output) {
+            if (possibleMeeting.overlaps(existingMeeting)) {
+              // Case 1: E |------|
+              //         P  |---|
+              //
+              // Case 2: E     |---|
+              //         P |----------|
+              //
+              // Case 3: E     |-------|
+              //         P  |------|
+              //
+              // Case 4: E |-----|
+              //         P   |-------|
+              if (existingMeeting.contains(possibleMeeting)) {
+                // Remove possibleMeeting
+              } else if (possibleMeeting.contains(existingMeeting)) {
+                // Split into two on either side
+              } else if (possibleMeeting.start() < existingMeeting.start()) {
+                // Split into one on left
+              } else if (possibleMeeting.end() > existingMeeting.end()) {
+                // Split into one on right
+              }
+            }
+          }
         }
       }
     }
+
+    return output;
     return Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TIME_0830AM, false),
             TimeRange.fromStartEnd(TIME_0900AM, TimeRange.END_OF_DAY, true));
   }
