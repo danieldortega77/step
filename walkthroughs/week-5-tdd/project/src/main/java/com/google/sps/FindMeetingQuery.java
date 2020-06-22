7baa0f37648c985077c789ab07882820b51527cf
Daniel Ortega <49536646+danieldortega77@users.noreply.github.com>
2020-06-22 15:42:15 -0700

    Week 5: Calendar Project (#15)
    Complete the query() method in FindMeetingQuery.java, and add functionality for optional attendees.
    
    Write test cases that check the functionality of optional attendees.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,95 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public final class FindMeetingQuery {
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    List<TimeRange> availableTimeRanges = Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true));
+
+    // Update output time slots for all mandatory attendees
+    for (String attendee : request.getAttendees()) {
+      availableTimeRanges = updateAvailableTimeRanges(events, attendee, availableTimeRanges);
+    }
+
+    List<TimeRange> mandatoryTimeRanges = cleanUpOutput(request, availableTimeRanges);
+    
+    // Update output time slots for all optional attendees
+    for (String attendee : request.getOptionalAttendees()) {
+      availableTimeRanges = updateAvailableTimeRanges(events, attendee, availableTimeRanges);
+    }
+
+    List<TimeRange> optionalTimeRanges = cleanUpOutput(request, availableTimeRanges);
+
+    if (optionalTimeRanges.size() == 0 && request.getAttendees().size() != 0) {
+      return mandatoryTimeRanges;
+    }
+    return optionalTimeRanges;
+  }
+
+  List<TimeRange> updateAvailableTimeRanges(Collection<Event> events, String attendee, List<TimeRange> availableTimeRanges) {
+    // For each time slot of that attendee
+    for (Event event : events) {
+      if (event.getAttendees().contains(attendee)) {
+        TimeRange existingTR = event.getWhen();
+        List<TimeRange> tempRanges = new ArrayList<TimeRange>();
+
+        // For each currently "open" time slot
+        for (TimeRange possibleTR : availableTimeRanges) {
+          if (possibleTR.overlaps(existingTR)) {
+            // Case 1: E |------|
+            //         P  |---|
+            //
+            // Case 2: E     |---|
+            //         P |----------|
+            //
+            // Case 3: E     |-------|
+            //         P  |------|
+            //
+            // Case 4: E |-----|
+            //         P   |-------|
+            int possibleStart = possibleTR.start();
+            int possibleEnd = possibleTR.end();
+            int existingStart = existingTR.start();
+            int existingEnd = existingTR.end();
+
+            if (existingTR.contains(possibleTR)) {
+              // Disregard possibleTR
+              continue;
+            } else if (possibleTR.contains(existingTR)) {
+              // Split into two on either side
+              tempRanges.add(TimeRange.fromStartEnd(possibleStart, existingStart, false));
+              tempRanges.add(TimeRange.fromStartEnd(existingEnd, possibleEnd, false));
+            } else if (possibleStart < existingStart) {
+              // Split into one on left
+              tempRanges.add(TimeRange.fromStartEnd(possibleStart, existingStart, false));
+            } else if (possibleEnd > existingEnd) {
+              // Split into one on right
+              tempRanges.add(TimeRange.fromStartEnd(existingEnd, possibleEnd, false));
+            }
+          } else {
+            tempRanges.add(possibleTR);
+          }
+        }
+        availableTimeRanges = tempRanges;
+      }
+    }
+    return availableTimeRanges;
+  }
+
+  // Remove any leftover time slots that have durations outside of [requestedDuration, infinity)
+  List<TimeRange> cleanUpOutput(MeetingRequest request, List<TimeRange> output) {
+    List<TimeRange> tempOutput = new ArrayList<TimeRange>();
+    for (TimeRange possibleTR : output) {
+      int duration = possibleTR.duration();
+      if (duration < request.getDuration()) {
+        continue;
+      }
+      tempOutput.add(possibleTR);
+    }
+    return tempOutput;
   }
 }
